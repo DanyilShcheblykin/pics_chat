88c0c917712232684efb28292edb257274cd2bc3
DanyilShcheblykin <danyil.shch@gmail.com>
2023-06-21 21:39:37 +0300

    Make logic of saving data to locStorage on reload

--- src/components/comment-area/comment-area.tsx
+++ src/components/comment-area/comment-area.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./comment-area.scss";
 import { CommentsData } from "../../App";
 
@@ -7,28 +7,47 @@ interface CommentAreaProps {
 }
 
 const CommentArea = ({ setComments }: CommentAreaProps) => {
-  const [text, setText] = useState<CommentsData>();
+  const [text, setText] = useState<string>(
+    localStorage.getItem("savedText") || "" 
+  );
+
+  const saveText = () => {
+    console.log("Saving text:", text);
+    localStorage.setItem("savedText", text ? text : "");
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", saveText);
+
+    return () => {
+      window.removeEventListener("beforeunload", saveText);
+    };
+  }, [text]);
 
   const addComment = () => {
     setComments((prev) => {
       if (prev && text) {
-        const newState = [...prev, text];
+        const newObj = { body: text };
+        const newState = [...prev, newObj];
         return newState;
       }
       return prev;
     });
+    setText("")
   };
 
   return (
     <section className="coment-area">
       <div className="coment-area-block">
         <textarea
-          value={text?.body}
-          onChange={(e) => setText({ body: e.target.value })}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           className="text-area"
           rows={5}
         ></textarea>
-        <button onClick={addComment} className="coment-button">Send</button>
+        <button onClick={addComment} className="coment-button">
+          Send
+        </button>
       </div>
     </section>
   );
